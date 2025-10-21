6625b62dadf2a578c9bd20cbf24e841da48cb602
Kaio <kakacnmendes@gmail.com>
2025-10-21 20:09:19 -0300

    adiciona o metodo de consultar um usuario pelo id

---
+++ src/main/java/br/com/kaio_app/gateway_academico/service/DiscenteService.java
@@ -0,0 +1,26 @@
+package br.com.kaio_app.gateway_academico.service;
+
+import br.com.kaio_app.gateway_academico.model.DiscenteDTO;
+import br.com.kaio_app.gateway_academico.repository.DiscenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DiscenteService {
+
+    // Depende APENAS do repositório
+    private final DiscenteRepository discenteRepository;
+
+    @Autowired
+    public DiscenteService(DiscenteRepository discenteRepository) {
+        this.discenteRepository = discenteRepository;
+    }
+
+    public Optional<DiscenteDTO> consultStudentData(Long id) {
+        // A lógica agora é simples: apenas busca no repositório em memória.
+        // O "DataLoader" já fez o trabalho de buscar na API.
+        return discenteRepository.findById(id);
+    }
+}
