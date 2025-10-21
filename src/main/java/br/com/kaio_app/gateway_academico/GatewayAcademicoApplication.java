97ee69d9f7511276c5b7ede8bfbdb765665a2670
Kaio <kakacnmendes@gmail.com>
2025-10-21 18:22:06 -0300

    adiciona as configuracoes iniciais de dependencia do projeto

---
+++ src/main/java/br/com/kaio_app/gateway_academico/GatewayAcademicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaio_app.gateway_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaio_app/gateway_academico/GatewayAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaio_app.gateway_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
