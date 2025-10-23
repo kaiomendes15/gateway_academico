91436229352320dad4484d2b7b6572a0af3ff735
Kaio <kakacnmendes@gmail.com>
2025-10-22 22:33:50 -0300

    adiciona a funcao para buscar por todos os discentes

--- src/main/java/br/com/kaio_app/gateway_academico/config/DataLoader.java
+++ src/main/java/br/com/kaio_app/gateway_academico/config/DataLoader.java
@@ -6,6 +6,7 @@ import br.com.kaio_app.gateway_academico.repository.DiscenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 
 import java.util.Map;
 
@@ -29,7 +30,7 @@ public class DataLoader implements CommandLineRunner {
             Map<Long, DiscenteDTO> discentes = discenteClient.getAll();
             discenteRepository.saveAll(discentes.values());
             System.out.println("Discentes carregados: " + discentes.size());
-        } catch (Exception e) {
+        } catch (RestClientException e) {
             System.err.println("Falha ao carregar Discentes: " + e.getMessage());
         }
     }
--- src/main/java/br/com/kaio_app/gateway_academico/controller/DiscenteController.java
+++ src/main/java/br/com/kaio_app/gateway_academico/controller/DiscenteController.java
@@ -6,18 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.Optional;
 
-@RestController("/discentes")
+@RestController
+@RequestMapping("/discentes")
 public class DiscenteController {
 
     @Autowired
     private DiscenteService discenteService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<DiscenteDTO> consultStudents(@PathVariable Long id) {
+    public ResponseEntity<DiscenteDTO> consultStudent(@PathVariable Long id) {
         // Lembrete -> O optional de um tipo em uma variável é declarar que
         // aquela variável é opcional, ou seja, ela pode receber um valor com
         // aquele tipo especificado ou null.
@@ -29,5 +32,15 @@ public class DiscenteController {
         } else {
             return ResponseEntity.notFound().build(); // Retorna 404 Not Found
         }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Collection<DiscenteDTO>> consultAllStudents() {
+
+        Collection<DiscenteDTO> students = discenteService.consultAllStudents();
+        if (students.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(students);
     }
 }
