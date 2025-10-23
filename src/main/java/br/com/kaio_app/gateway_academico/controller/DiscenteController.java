bfd1797031e290d557d15f7549f7961f1bd4e6f9
Kaio <kakacnmendes@gmail.com>
2025-10-22 22:45:36 -0300

    adiciona as respostas de erro da api para consultas de discentes

--- src/main/java/br/com/kaio_app/gateway_academico/controller/DiscenteController.java
+++ src/main/java/br/com/kaio_app/gateway_academico/controller/DiscenteController.java
@@ -1,8 +1,11 @@
 package br.com.kaio_app.gateway_academico.controller;
 
+import br.com.kaio_app.gateway_academico.model.ApiError;
 import br.com.kaio_app.gateway_academico.model.DiscenteDTO;
 import br.com.kaio_app.gateway_academico.service.DiscenteService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -20,7 +24,8 @@ public class DiscenteController {
     private DiscenteService discenteService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<DiscenteDTO> consultStudent(@PathVariable Long id) {
+    public ResponseEntity<Object> consultStudent(@PathVariable Long id,
+                                            HttpServletRequest request) {
         // Lembrete -> O optional de um tipo em uma variável é declarar que
         // aquela variável é opcional, ou seja, ela pode receber um valor com
         // aquele tipo especificado ou null.
@@ -30,16 +35,28 @@ public class DiscenteController {
         if (student.isPresent()) {
             return ResponseEntity.ok(student.get()); // Retorna 200 OK
         } else {
-            return ResponseEntity.notFound().build(); // Retorna 404 Not Found
+            ApiError errorResponse = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    "Dados não foram encontrados.",
+                    "Discente com ID " + id + " não foi encontrado.",
+                    request.getRequestURI()
+            );
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse); // Retorna 404 Not Found
         }
     }
 
     @GetMapping("")
-    public ResponseEntity<Collection<DiscenteDTO>> consultAllStudents() {
+    public ResponseEntity<Object> consultAllStudents(HttpServletRequest request) {
 
         Collection<DiscenteDTO> students = discenteService.consultAllStudents();
         if (students.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            ApiError errorResponse = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    "Dados não foram encontrados.",
+                    "Nenhum discente foi encontrado no sistema.",
+                    request.getRequestURI()
+            );
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
         return ResponseEntity.ok(students);
     }
