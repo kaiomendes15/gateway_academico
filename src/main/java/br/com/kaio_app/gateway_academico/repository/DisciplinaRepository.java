216ff85d7b69c2a0c3791250d54352ddd3433be7
Kaio <kakacnmendes@gmail.com>
2025-10-25 18:56:12 -0300

    adiciona o repositorio de disciplinas

---
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/DisciplinaRepository.java
@@ -0,0 +1,8 @@
+package br.com.kaio_app.gateway_academico.repository;
+
+import br.com.kaio_app.gateway_academico.model.DisciplinaDTO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DisciplinaRepository extends InMemoryRepository<DisciplinaDTO>{
+}
