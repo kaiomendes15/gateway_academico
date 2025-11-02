ef6cd749a21333554685577b484a77c22a9a72ad
Kaio <kakacnmendes@gmail.com>
2025-11-02 15:13:20 -0300

    adiciona o repositorio que ira se comunicar com a 'tabela' de relacao aluno-disciplina

---
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/RelacaoMatriculaRepository.java
@@ -0,0 +1,6 @@
+package br.com.kaio_app.gateway_academico.repository;
+
+import br.com.kaio_app.gateway_academico.model.Identifiable;
+
+public class RelacaoMatriculaRepository<T extends Identifiable> {
+}
