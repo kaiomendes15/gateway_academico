2fcd82b33bbae01d6ed519ac2a8faf0273da3e7c
Kaio <kakacnmendes@gmail.com>
2025-11-04 18:37:47 -0300

    adiciona o repositorio responsavel pela manipulacao de dados da relacao aluno_disciplina

---
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/RelationDiscenteDisciplinaRepository.java
@@ -0,0 +1,7 @@
+package br.com.kaio_app.gateway_academico.repository;
+
+import br.com.kaio_app.gateway_academico.model.DiscenteDTO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RelationDiscenteDisciplinaRepository extends InMemoryRelationRepository<DiscenteDTO> { }
