093f631d0d2b74d604cf0c24bd9b4cf6558447b9
Kaio <kakacnmendes@gmail.com>
2025-11-05 22:52:44 -0300

    implementa as excecoes na MatriculaService

--- src/main/java/br/com/kaio_app/gateway_academico/domain/service/MatriculaService.java
+++ src/main/java/br/com/kaio_app/gateway_academico/domain/service/MatriculaService.java
@@ -2,6 +2,7 @@ package br.com.kaio_app.gateway_academico.domain.service;
 
 import br.com.kaio_app.gateway_academico.domain.model.DiscenteDTO;
 import br.com.kaio_app.gateway_academico.domain.model.DisciplinaDTO;
+import br.com.kaio_app.gateway_academico.domain.model.exceptions.*;
 import br.com.kaio_app.gateway_academico.repository.DiscenteRepository;
 import br.com.kaio_app.gateway_academico.repository.DisciplinaRepository;
 import br.com.kaio_app.gateway_academico.repository.RelationDiscenteDisciplinaRepository;
@@ -36,31 +37,33 @@ public class MatriculaService {
 
 
         if (discentes.isEmpty()) {
-            // lancar erro
+            throw new RecursoNaoEncontradoException("Discente com ID " + discenteId + " não " +
+                    "encontrado.");
         }
 
         if (disciplinas.isEmpty()) {
-            // lancar erro
+            throw new RecursoNaoEncontradoException("Disciplina com ID " + disciplinaId + " não " +
+                    "encontrada.");
         }
 
         if (discentes.get().getStatus().equals("Trancado")) {
-            // lancar erro
+            throw new AlunoTrancadoException("O discente " + discentes.get().getNome() + " não pode se matricular pois seu status é 'Trancado'.");
         }
 
         if (!discentes.get().getCurso().equals(disciplinas.get().getCurso())) {
-            // lancar erro
+            throw new CursoIncompativelException("A disciplina '" + disciplinas.get().getNome() + "' não pertence ao curso do discente (" + discentes.get().getCurso() + ").");
         }
 
         if (disciplinas.get().getVagas() <= 0) {
-            // lancar erro
+            throw new DisciplinaSemVagasException("A disciplina '" + disciplinas.get().getNome() + "' não possui vagas disponíveis.");
         }
 
         if (matriculaRepository.countByDiscenteId(discenteId) >= 5) {
-            // lancar erro
+            throw new LimiteMatriculaException("Limite de 5 matrículas simultâneas atingido.");
         }
 
         if (matriculaRepository.discenteContainItem(discenteId, disciplinaId)) {
-            // lancar erro
+            throw new AlunoJaMatriculadoException("O discente já está matriculado nesta disciplina.");
         }
 
         matriculaRepository.addItemToList(discenteId, disciplinaId);
