84ca33aad78f3fe04bff372cf2dec51e1f54b027
Kaio <kakacnmendes@gmail.com>
2025-11-05 22:41:05 -0300

    adiciona a ResourceNotFoundException

--- src/main/java/br/com/kaio_app/gateway_academico/domain/model/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.kaio_app.gateway_academico.domain.model.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
