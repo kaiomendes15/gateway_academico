b22fb40a562079c770449ff0c363394d8957095e
Kaio <kakacnmendes@gmail.com>
2025-11-05 22:40:19 -0300

    adiciona a AlunoTrancadoException

---
+++ src/main/java/br/com/kaio_app/gateway_academico/domain/model/exceptions/AlunoTrancadoException.java
@@ -0,0 +1,7 @@
+package br.com.kaio_app.gateway_academico.domain.model.exceptions;
+
+public class AlunoTrancadoException extends RuntimeException {
+    public AlunoTrancadoException(String message) {
+        super(message);
+    }
+}
