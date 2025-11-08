083e3239b9a28fc0313f9835b337d3f8ecd169b9
Kaio <kakacnmendes@gmail.com>
2025-11-08 08:23:50 -0300

    adiciona o metodo para listar as disciplinas que o aluno esta matriculado

--- src/main/java/br/com/kaio_app/gateway_academico/controller/MatriculaController.java
+++ src/main/java/br/com/kaio_app/gateway_academico/controller/MatriculaController.java
@@ -1,11 +1,15 @@
 package br.com.kaio_app.gateway_academico.controller;
 
+import br.com.kaio_app.gateway_academico.domain.model.DisciplinaDTO;
 import br.com.kaio_app.gateway_academico.domain.service.MatriculaService;
+import br.com.kaio_app.gateway_academico.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("matricula")
@@ -13,6 +17,8 @@ public class MatriculaController {
 
     @Autowired
     private MatriculaService matriculaService;
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
 
     @PostMapping("/{discenteId}/{disciplinaId}")
     public ResponseEntity<Void> matricular(@PathVariable Long discenteId,
@@ -23,10 +29,9 @@ public class MatriculaController {
     }
 
     @GetMapping("/{discenteId}")
-    public ResponseEntity<List<Long>> exibirDisciplinasMatriculadas(@PathVariable Long discenteId) {
-        List<Long> disciplinas =
+    public ResponseEntity<Map<Long, DisciplinaDTO>> exibirDisciplinasMatriculadas(@PathVariable Long discenteId) {
+        Map<Long, DisciplinaDTO> disciplinas =
                 matriculaService.exibirMatriculaAluno(discenteId);
-
         return ResponseEntity.ok().body(disciplinas);
     }
 }
