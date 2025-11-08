fc1338ef193898b834b43cfcb8a3d6d54f70f02c
Kaio <kakacnmendes@gmail.com>
2025-11-08 14:09:54 -0300

    corrige as tratativas de erro para serem gerenciadas pelo global exception handler

--- src/main/java/br/com/kaio_app/gateway_academico/controller/DisciplinaController.java
+++ src/main/java/br/com/kaio_app/gateway_academico/controller/DisciplinaController.java
@@ -26,36 +26,16 @@ public class DisciplinaController {
         // Lembrete -> O optional de um tipo em uma variável é declarar que
         // aquela variável é opcional, ou seja, ela pode receber um valor com
         // aquele tipo especificado ou null.
-        Optional<DisciplinaDTO> discipline =
+        Optional<DisciplinaDTO> disciplina =
                 disciplinaService.consultDisciplineData(id);
-
-        if (discipline.isPresent()) {
-            return ResponseEntity.ok(discipline.get()); // Retorna 200 OK
-        } else {
-            ApiError errorResponse = new ApiError(
-                    HttpStatus.NOT_FOUND.value(),
-                    "Dados não foram encontrados.",
-                    "Disciplina com ID " + id + " não foi encontrada.",
-                    request.getRequestURI()
-            );
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse); // Retorna 404 Not Found
-        }
+        return ResponseEntity.ok(disciplina.get());
     }
 
     @GetMapping("")
     public ResponseEntity<Object> consultAllDisciplines(HttpServletRequest request) {
 
-        Collection<DisciplinaDTO> discipline =
+        Collection<DisciplinaDTO> disciplinas =
                 disciplinaService.consultAllDisciplines();
-        if (discipline.isEmpty()) {
-            ApiError errorResponse = new ApiError(
-                    HttpStatus.NOT_FOUND.value(),
-                    "Dados não foram encontrados.",
-                    "Nenhuma disciplina foi encontrado no sistema.",
-                    request.getRequestURI()
-            );
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-        }
-        return ResponseEntity.ok(discipline);
+        return ResponseEntity.ok(disciplinas);
     }
 }
