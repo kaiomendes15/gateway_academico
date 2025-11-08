ebf9d2eafd443f8cee184f74597c37811917d6cc
Kaio <kakacnmendes@gmail.com>
2025-11-08 08:25:12 -0300

    adiciona os metodos de incrementar e decrementar total de vagas para as disciplinas

--- src/main/java/br/com/kaio_app/gateway_academico/repository/DisciplinaRepository.java
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/DisciplinaRepository.java
@@ -3,6 +3,20 @@ package br.com.kaio_app.gateway_academico.repository;
 import br.com.kaio_app.gateway_academico.domain.model.DisciplinaDTO;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class DisciplinaRepository extends InMemoryRepository<DisciplinaDTO>{
+
+    public void decrementarVagas(Long disciplinaId){
+        Optional<DisciplinaDTO> disciplina = this.findById(disciplinaId);
+
+        disciplina.ifPresent(disciplinaDTO -> disciplinaDTO.setVagas(disciplinaDTO.getVagas() - 1));
+    }
+
+    public void incrementarVagas(Long disciplinaId){
+        Optional<DisciplinaDTO> disciplina = this.findById(disciplinaId);
+
+        disciplina.ifPresent(disciplinaDTO -> disciplinaDTO.setVagas(disciplinaDTO.getVagas() + 1));
+    }
 }
