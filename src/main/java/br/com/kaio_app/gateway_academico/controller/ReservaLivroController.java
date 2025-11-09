5f9151f1f2e9adac256bf431449f9e2656e59a00
Kaio <kakacnmendes@gmail.com>
2025-11-09 15:28:55 -0300

    adiciona rota para reservar e visualizar livros reservador de um aluno

--- src/main/java/br/com/kaio_app/gateway_academico/controller/ReservaLivroController.java
+++ src/main/java/br/com/kaio_app/gateway_academico/controller/ReservaLivroController.java
@@ -1,13 +1,13 @@
 package br.com.kaio_app.gateway_academico.controller;
 
+import br.com.kaio_app.gateway_academico.domain.model.LivroDTO;
 import br.com.kaio_app.gateway_academico.domain.service.ReservaLivroService;
 import br.com.kaio_app.gateway_academico.repository.RelacaoDiscenteLivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/reservar")
@@ -16,13 +16,18 @@ public class ReservaLivroController {
     @Autowired
     private ReservaLivroService reservaLivroService;
 
-    @Autowired
-    private RelacaoDiscenteLivroRepository reservaRepository;
 
     @PostMapping("/{discenteId}/{livroId}")
     public ResponseEntity<Void> reservar(@PathVariable Long discenteId,
                                          @PathVariable Long livroId) {
         reservaLivroService.reservarLivro(discenteId, livroId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{discenteId}")
+    public ResponseEntity<Map<Long, LivroDTO>> exibirLivrosReservados(@PathVariable Long discenteId) {
+        Map<Long, LivroDTO> livros = reservaLivroService.exibirLivrosReservados(discenteId);
+
+        return ResponseEntity.ok().body(livros);
     }
 }
