e370f3c7df25b0ba7a81f4f2ba0b79f0c914594d
Kaio <kakacnmendes@gmail.com>
2025-11-09 14:17:35 -0300

    adiciona o metodo de exibicao de livros reservador por um discente

--- src/main/java/br/com/kaio_app/gateway_academico/domain/service/ReservaLivroService.java
+++ src/main/java/br/com/kaio_app/gateway_academico/domain/service/ReservaLivroService.java
@@ -3,8 +3,7 @@ package br.com.kaio_app.gateway_academico.domain.service;
 import br.com.kaio_app.gateway_academico.domain.model.DiscenteDTO;
 import br.com.kaio_app.gateway_academico.domain.model.DisciplinaDTO;
 import br.com.kaio_app.gateway_academico.domain.model.LivroDTO;
-import br.com.kaio_app.gateway_academico.domain.model.exceptions.AlunoStatusInvalidoException;
-import br.com.kaio_app.gateway_academico.domain.model.exceptions.RecursoNaoEncontradoException;
+import br.com.kaio_app.gateway_academico.domain.model.exceptions.*;
 import br.com.kaio_app.gateway_academico.repository.DiscenteRepository;
 import br.com.kaio_app.gateway_academico.repository.LivroRepository;
 import br.com.kaio_app.gateway_academico.repository.RelacaoDiscenteLivroRepository;
@@ -31,7 +30,41 @@ public class ReservaLivroService {
         this.discenteRepository = discenteRepository;
     }
 
-    public void reservarLivro(Long discenteId, Long livroId) {}
+    public void reservarLivro(Long discenteId, Long livroId) {
+        Optional<DiscenteDTO> discentes =
+                discenteRepository.findById(discenteId);
+        Optional<LivroDTO> livro =
+                livroRepository.findById(livroId);
+
+        if (discentes.isEmpty()) {
+            throw new RecursoNaoEncontradoException("Discente com ID " + discenteId + " não " +
+                    "encontrado.");
+        }
+
+        if (discentes.get().getStatus().equals("Trancado")) {
+            throw new AlunoStatusInvalidoException("O discente " + discentes.get().getNome() + " está com o curso trancado, logo não possui livros reservados.");
+        }
+
+        if (discentes.get().getStatus().equals("Cancelado")) {
+            throw new AlunoStatusInvalidoException("O discente " + discentes.get().getNome() + " realizou o cancelamento de sua graduação, logo não possui livros reservados.");
+        }
+
+        if (livro.get().getStatus().equals("Indisponível")) {
+            throw new LivroIndisponivelException("O livro com ID " + livroId +
+                    " está indisponível para reserva.");
+        }
+
+        if (reservaLivroRepository.countByDiscenteId(discenteId) >= 5) {
+            throw new LimiteReservaException("Limite de 5 livros reservados " +
+                    "simultâneamente atingido.");
+        }
+
+        if (reservaLivroRepository.discenteContainItem(discenteId, livroId)) {
+            throw new LivroJaReservadoException("O livro com ID " + livroId + " já foi reservado pelo discente " + discentes.get().getNome());
+        }
+
+
+    }
     public void cancelarReservaLivro(Long discenteId, Long livroId) {}
     public Map<Long, LivroDTO> exibirLivrosReservados(Long discenteId) {
         Optional<DiscenteDTO> discentes =
