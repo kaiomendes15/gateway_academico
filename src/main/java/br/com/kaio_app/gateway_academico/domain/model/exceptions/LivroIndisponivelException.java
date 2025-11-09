6dc923281219b8c8d970819f2e5ac3f8cd91e33f
Kaio <kakacnmendes@gmail.com>
2025-11-09 14:16:11 -0300

    adiciona a excecao de livro indisponivel

---
+++ src/main/java/br/com/kaio_app/gateway_academico/domain/model/exceptions/LivroIndisponivelException.java
@@ -0,0 +1,7 @@
+package br.com.kaio_app.gateway_academico.domain.model.exceptions;
+
+public class LivroIndisponivelException extends RuntimeException {
+  public LivroIndisponivelException(String message) {
+    super(message);
+  }
+}
