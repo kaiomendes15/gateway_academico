6deb8b7c2a9259fa0943e1444799d9833eb13743
Kaio <kakacnmendes@gmail.com>
2025-11-09 13:51:45 -0300

    adiciona o repositorio da relacao discente_livro

---
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/RelacaoDiscenteLivroRepository.java
@@ -0,0 +1,8 @@
+package br.com.kaio_app.gateway_academico.repository;
+
+import br.com.kaio_app.gateway_academico.domain.model.LivroDTO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RelacaoDiscenteLivroRepository extends InMemoryRelationRepository<LivroDTO>{
+}
