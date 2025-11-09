c7c74355cabf9ba1b310c848abcc9717b7c024a5
Kaio <kakacnmendes@gmail.com>
2025-11-09 14:16:59 -0300

    adicona a excecao de livro ja reservado

--- src/main/java/br/com/kaio_app/gateway_academico/domain/model/exceptions/LivroJaReservadoException.java
+++ src/main/java/br/com/kaio_app/gateway_academico/domain/model/exceptions/LivroJaReservadoException.java
@@ -1,7 +1,7 @@
 package br.com.kaio_app.gateway_academico.domain.model.exceptions;
 
 public class LivroJaReservadoException extends RuntimeException {
-  public LivroJaReservadoException(String message) {
-    super(message);
-  }
+    public LivroJaReservadoException(String message) {
+        super(message);
+    }
 }
