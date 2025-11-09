86148dc0a4ab69de6f59ae43da52d4d5836abf81
Kaio <kakacnmendes@gmail.com>
2025-11-09 15:29:20 -0300

    Altera o tipo generico do repositorio em memoria

--- src/main/java/br/com/kaio_app/gateway_academico/repository/RelacaoDiscenteLivroRepository.java
+++ src/main/java/br/com/kaio_app/gateway_academico/repository/RelacaoDiscenteLivroRepository.java
@@ -1,8 +1,9 @@
 package br.com.kaio_app.gateway_academico.repository;
 
+import br.com.kaio_app.gateway_academico.domain.model.DiscenteDTO;
 import br.com.kaio_app.gateway_academico.domain.model.LivroDTO;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RelacaoDiscenteLivroRepository extends InMemoryRelationRepository<LivroDTO>{
+public class RelacaoDiscenteLivroRepository extends InMemoryRelationRepository<DiscenteDTO>{
 }
